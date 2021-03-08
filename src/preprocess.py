dd83a27ce3d9d01c7b4f376f0fcbb87085a1aa7b
zyli93 <zeyuli@ucla.edu>
2021-03-07 18:33:08 -0800

    add InstanceFeatures and comments for notebook

---
+++ src/preprocess.py
@@ -0,0 +1,11 @@
+"""
+    Preprocess input CPC file and output InstanceFeatures
+
+    Authors:
+        Zihan Liu <leoliu00529@gmail.com>
+        Zeyu Li <zyli@cs.ucla.edu>
+    Date created: March 7, 2021
+    Python version: 3.6.0+
+"""
+
+# TODO: please add preprocessing code to this file
